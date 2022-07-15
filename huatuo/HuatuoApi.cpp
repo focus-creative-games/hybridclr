04a5df0e0e983140006c27d648cccba1a66a457b
walon <taojingjian@gmail.com>
2022-07-15 22:53:27 +0800

    [fix] 修复HuatuoApi的extern c函数未指定 call convension导致在x86平台下pinvoke失败的bug

--- huatuo/HuatuoApi.cpp
+++ huatuo/HuatuoApi.cpp
@@ -6,27 +6,27 @@
 extern "C"
 {
 
-	IL2CPP_EXPORT int32_t HuatuoApi_LoadMetadataForAOTAssembly(void* dllBytes, uint32_t dllSize)
+	IL2CPP_EXPORT int32_t DEFAULT_CALL HuatuoApi_LoadMetadataForAOTAssembly(void* dllBytes, uint32_t dllSize)
 	{
 		return huatuo::metadata::AOTHomologousImage::LoadMetadataForAOTAssembly(dllBytes, dllSize);
 	}
 
-	IL2CPP_EXPORT uint32_t HuatuoApi_GetInterpreterThreadObjectStackSize()
+	IL2CPP_EXPORT uint32_t DEFAULT_CALL HuatuoApi_GetInterpreterThreadObjectStackSize()
 	{
 		return huatuo::HuatuoConfig::GetInterpreterThreadObjectStackSize();
 	}
 
-	IL2CPP_EXPORT void HuatuoApi_SetInterpreterThreadObjectStackSize(uint32_t size)
+	IL2CPP_EXPORT void DEFAULT_CALL HuatuoApi_SetInterpreterThreadObjectStackSize(uint32_t size)
 	{
 		huatuo::HuatuoConfig::SetInterpreterThreadObjectStackSize(size);
 	}
 
-	IL2CPP_EXPORT uint32_t HuatuoApi_GetInterpreterThreadFrameStackSize()
+	IL2CPP_EXPORT uint32_t DEFAULT_CALL HuatuoApi_GetInterpreterThreadFrameStackSize()
 	{
 		return huatuo::HuatuoConfig::GetInterpreterThreadFrameStackSize();
 	}
 
-	IL2CPP_EXPORT void HuatuoApi_SetInterpreterThreadFrameStackSize(uint32_t size)
+	IL2CPP_EXPORT void DEFAULT_CALL HuatuoApi_SetInterpreterThreadFrameStackSize(uint32_t size)
 	{
 		huatuo::HuatuoConfig::SetInterpreterThreadFrameStackSize(size);
 	}
