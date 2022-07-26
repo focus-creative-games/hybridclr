e824375e08dbcc8257c7050feb02e9dd94a097ac
walon <walon@focus-creative-games.com>
2022-07-26 20:32:36 +0800

    [refactor] 重构 ReversePInvokeMethodStub.cpp，消除重复代码

--- huatuo/metadata/ReversePInvokeMethodStub.cpp
+++ huatuo/metadata/ReversePInvokeMethodStub.cpp
@@ -6,23 +6,22 @@ namespace huatuo
 namespace metadata
 {
 	//!!!{{REVERSE_PINVOKE_METHOD_STUB
+	
+	void CallLuaFunction(void* xState, int32_t wrapperIndex)
+	{
+		const MethodInfo* method = MetadataModule::GetMethodInfoByReversePInvokeWrapperIndex(wrapperIndex);
+		typedef void (*Callback)(void* xState, const MethodInfo* method);
+		((Callback)GetInterpreterDirectlyCallMethodPointer(method))(xState, method);
+	}
 
 	void __ReversePInvokeMethod_0(void* xState)
 	{
-		// for xLua developer
-		int32_t wrapperIndex = 0;
-		const MethodInfo* method = MetadataModule::GetMethodInfoByReversePInvokeWrapperIndex(wrapperIndex);
-		typedef void (*Callback)(void* xState, const MethodInfo* method);
-		((Callback)method->methodPointer)(xState, method);
+		CallLuaFunction(xState, 0);
 	}
 
 	void __ReversePInvokeMethod_1(void* xState)
 	{
-		// for xLua developer
-		int32_t wrapperIndex = 1;
-		const MethodInfo* method = MetadataModule::GetMethodInfoByReversePInvokeWrapperIndex(wrapperIndex);
-		typedef void (*Callback)(void* xState, const MethodInfo* method);
-		((Callback)method->methodPointer)(xState, method);
+		CallLuaFunction(xState, 1);
 	}
 
 	Il2CppMethodPointer s_ReversePInvokeMethodStub[]
