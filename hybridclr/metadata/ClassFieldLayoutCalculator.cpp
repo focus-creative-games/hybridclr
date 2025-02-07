focus-creative-games/hybridclr

commit 199b1b1a789d760828bd33e7e1438261cd1f8d15

[fix] fix the bug in ClassFieldLayoutCalculator::LayoutFields where the alignment calculation incorrectly considers naturalAlignment, resulting in field offsets that are inconsistent with the actual field offsets in AOT. This bug originates from IL2CPP itself and only occurs in Unity 2021 and earlier versions.

hybridclr/metadata/ClassFieldLayoutCalculator.cpp
@@ -194,8 +194,8 @@ namespace metadata
             // is less than the compiler's minimum alignment (4 bytes), lets use the natural alignment if we have it.
             uint8_t alignment = sa.alignment;
 #if !HYBRIDCLR_UNITY_2022_OR_NEW
-            if (alignment < 4 && sa.naturalAlignment != 0)
-                alignment = sa.naturalAlignment;
+            //if (alignment < 4 && sa.naturalAlignment != 0)
+            //    alignment = sa.naturalAlignment;
 #endif
             if (packing != 0)
                 alignment = std::min(sa.alignment, packing);

commit d4d7a97676e351c8dd6c0819f5d3798429029ba0

[fix] fix bug of computing explicit struct layout caused by commit "199b1b1a789d760828bd33e7e1438261cd1f8d15"

hybridclr/metadata/ClassFieldLayoutCalculator.cpp
@@ -194,8 +194,8 @@ namespace metadata
             // is less than the compiler's minimum alignment (4 bytes), lets use the natural alignment if we have it.
             uint8_t alignment = sa.alignment;
 #if !HYBRIDCLR_UNITY_2022_OR_NEW
-            //if (alignment < 4 && sa.naturalAlignment != 0)
-            //    alignment = sa.naturalAlignment;
+            if (alignment < 4 && sa.naturalAlignment != 0)
+                alignment = sa.naturalAlignment;
 #endif
             if (packing != 0)
                 alignment = std::min(sa.alignment, packing);
@@ -359,6 +359,8 @@ namespace metadata
             }
             layout.alignment = maxAlignment;
 #if !HYBRIDCLR_UNITY_2022_OR_NEW
+            // in unity 2021- version, il2cpp force alignment to 1 for explicit layout
+            layout.alignment = 1;
             layout.naturalAlignment = layout.alignment;
 #endif
             layout.actualSize = layout.instanceSize = AlignTo(instanceSize, layout.alignment);