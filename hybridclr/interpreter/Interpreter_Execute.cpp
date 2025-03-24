1d6626bf996fb21252c797223302559f8459f7f7
walon <walon@code-philosophy.com>
2025-03-24 19:30:09 +0800

    [change] remove unnecessary `frame->ip = (byte*)ip;` assignment in LOAD_PREV_FRAME()

--- hybridclr/interpreter/Interpreter_Execute.cpp
+++ hybridclr/interpreter/Interpreter_Execute.cpp
@@ -1267,7 +1267,6 @@ namespace interpreter
 #define LOAD_PREV_FRAME() { \
 	imi = (const InterpMethodInfo*)frame->method->interpData; \
 	ip = frame->ip; \
-	frame->ip = (byte*)ip; \
 	ipBase = imi->codes; \
 	localVarBase = frame->stackBasePtr; \
 }
